f4e83a39cca4ce936c8c11b2fc4902001b9d1061
NyX-K1 <nyx59247@gmail.com>
2026-03-21 13:45:16 +0530

    chore: clean up development scripts, unused components, and API keys

--- src/pages/BalancedThoughtPage.tsx
+++
@@ -1,5 +0,0 @@
-import BalancedThoughtGenerator from '@/components/ui/balanced-thought-generator';
-
-export default function BalancedThoughtPage() {
-    return <BalancedThoughtGenerator standalone={true} />;
-}
